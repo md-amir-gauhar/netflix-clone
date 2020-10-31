e04fb1236e63865128f2681c576ca89674201694
md-amir-gauhar <gauharamir15@gmail.com>
2020-10-31 12:00:11 +0530

    completed banner, now creating navbar

--- src/Banner.js
+++ src/Banner.js
@@ -20,6 +20,10 @@ function Banner() {
     fetchData();
   }, []);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  }
+
   return (
     <header
       style={{
@@ -32,14 +36,18 @@ function Banner() {
       className="banner"
     >
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
 
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{movie?.overview}</h1>
+        <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
       </div>
+
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 }
