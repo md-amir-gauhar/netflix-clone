ea1467631770d856b6a235a41d80977e3a5cef75
md-amir-gauhar <gauharamir15@gmail.com>
2020-10-31 12:33:18 +0530

    navbar completed

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import './Nav.css'
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className={`nav ${show && 'nav__black'}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="Netflix Logo"
+      />
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
